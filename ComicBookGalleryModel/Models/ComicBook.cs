a0e63311d09a6d133d7c4f18cd52dbc9f61885e0
nerdyrasa <nerdyrasa@users.noreply.github.com>
2016-12-17 08:50:46 -0600

    Add properties to ComicBookEntity. Create a context. Use the context in the program.

--- ComicBookGalleryModel/Models/ComicBook.cs
+++ ComicBookGalleryModel/Models/ComicBook.cs
@@ -8,5 +8,11 @@ namespace ComicBookGalleryModel.Models
 {
     public class ComicBook
     {
+        public int Id { get; set; }
+        public string SeriesTitle { get; set; }
+        public int IssueNumber { get; set; }
+        public string Description { get; set; }
+        public DateTime PublishedOn { get; set; }
+
     }
 }
